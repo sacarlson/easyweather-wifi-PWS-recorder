857a95fa3928080a804854222824cfb167162190
sacarlson <freenet.surething@gmail.com>
2025-08-12 18:12:49 +0700

    Update config_example.py
    added config params

--- config_example.py
+++ config_example.py
@@ -8,11 +8,20 @@ config.read('config.ini')
 
 # Access values from sections
 print(f"id_user: {config['USER']['id_user']}")
+print(f"password: {config['USER']['password']}")
 print(f"Database filename: {config['DATABASE']['filename']}")
 print(f"Server Port: {config['SERVER']['port']}")
+print(f"Server path: {config['SERVER']['path']}")
+print(f"Server host: {config['SERVER']['host']}")
+
 
 # Access values with type conversion
 print(f"Server Port (int): {config.getint('SERVER', 'port')}")
+print(f"Server enable_repeater: {config.getboolean('SERVER', 'enable_repeater')}")
+print(f"Server send_corrected_rain: {config.getboolean('SERVER', 'send_corrected_rain')}")
+
+if config.getboolean('SERVER', 'enable_repeater'):
+    print("enable _repeater true")
 
 # List all sections
 print(f"Sections: {config.sections()}")
